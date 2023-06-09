cbe63a5a4cf61f1f0a19a8a93c9a67b625749136
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-09 10:07:34 +0000

    update

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -33,8 +33,8 @@ export default function Projects() {
         <title>Projects - JianyueHugo</title>
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        title="Things I provide and make myself"
+        intro="Some open source projects from other developers and I put it into service. Also some project made by my own or translated it "
       >
         <ul
           role="list"
