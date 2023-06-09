e3f0d5c4519f537693d915f57fcfcb69970fcc71
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-09 11:50:04 +0000

    2023.6.9-2050 change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ export function Footer() {
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/collection">Collection</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/other">Other</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} JianyueLab. All rights reserved.
