ae194b7ff2e0a1ead1b9624beae375e019f79e2c
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-09 05:54:28 +0000

    change

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,16 +7,8 @@ import logoAnimaginary from '@/images/logos/animaginary.svg'
 import logoCosmos from '@/images/logos/cosmos.svg'
 import logoHelioStream from '@/images/logos/helio-stream.svg'
 import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects = [
-  {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
-  },
   {
     name: 'Animaginary',
     description:
