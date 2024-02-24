98e2ae0073b1ef11e053fb9728b0e7927f3d43c4
JianyueHugo <valor@jianyuelab.cc>
2024-02-24 14:14:02 +0900

    fixing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -202,7 +202,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          JianyueHugo - Server Developer, Translator and Student
+          JianyueHugo
         </title>
         <meta
           name="description"
@@ -212,7 +212,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Server Developer, Translator, Game Player and Student
+            Server Developer, Translator and Student
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I am JianyueHugo, aka Valor, a student and a developer in Tokyo. I am a member of the translation group of <a href="https://www.tecostudio.cn" class="italic">TecoStudio</a>.
