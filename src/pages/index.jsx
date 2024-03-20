d415e3331a6346b1e9d1e7d926a57165d79064ab
JianyueLab <valor@jianyuelab.cc>
2024-03-20 22:58:39 +0900

    add works & orgs section and update study section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,35 +15,14 @@ import {
 } from '@/components/SocialIcons'
 import ib from '@/images/logos/ib.png'
 import kojimachi from '@/images/logos/kojimachi.png'
+import teco from '@/images/logos/teco.png'
+import nic from '@/images/logos/nic.png'
 import image1 from '@/images/photos/image_1.png'
 import image2 from '@/images/photos/banner.jpg'
 import image3 from '@/images/photos/image_2.png'
 import { formatDate } from '@/lib/formatDate'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
-
-function MailIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
 
 function BriefcaseIcon(props) {
   return (
@@ -63,19 +42,6 @@ function BriefcaseIcon(props) {
       <path
         d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
         className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
       />
     </svg>
   )
@@ -104,8 +70,15 @@ function SocialLink({ icon: Icon, ...props }) {
   )
 }
 
-function Resume() {
+function Study() {
   let resume = [
+    {
+      company: 'NUCB International College',
+      title: 'Student',
+      logo: nic,
+      start: '2024',
+      end: 'present'
+    },
     {
       company: 'Kojimachi Junior High School',
       title: 'Student',
@@ -126,7 +99,7 @@ function Resume() {
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work & Study</span>
+        <span className="ml-3">Study</span>
       </h2>
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
@@ -135,7 +108,62 @@ function Resume() {
               <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
             </div>
             <dl className="flex flex-auto flex-wrap gap-x-2">
-              <dt className="sr-only">Work & Study</dt>
+              <dt className="sr-only">Study</dt>
+              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                {role.company}
+              </dd>
+              <dt className="sr-only">Role</dt>
+              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                {role.title}
+              </dd>
+              <dt className="sr-only">Date</dt>
+              <dd
+                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                aria-label={`${role.start.label ?? role.start} until ${
+                  role.end.label ?? role.end
+                }`}
+              >
+                <time dateTime={role.start.dateTime ?? role.start}>
+                  {role.start.label ?? role.start}
+                </time>{' '}
+                <span aria-hidden="true">—</span>{' '}
+                <time dateTime={role.end.dateTime ?? role.end}>
+                  {role.end.label ?? role.end}
+                </time>
+              </dd>
+            </dl>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+function Works() {
+  let resume = [
+    {
+      company: 'TecoStudio (tecostudio.cn)',
+      title: 'Chief technology officer',
+      logo: teco,
+      start: '2023',
+      end: 'present',
+    },
+  ]
+
+  return (
+    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <BriefcaseIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Works & Orgs</span>
+      </h2>
+      <ol className="mt-6 space-y-4">
+        {resume.map((role, roleIndex) => (
+          <li key={roleIndex} className="flex gap-4">
+            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
+            </div>
+            <dl className="flex flex-auto flex-wrap gap-x-2">
+              <dt className="sr-only">Works & Orgs</dt>
               <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
                 {role.company}
               </dd>
@@ -202,7 +230,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I am JianyueHugo, aka Valor, a student and a developer in Tokyo. I am a member of TecoStudio."
+          content="I am JianyueHugo, aka Valor, a student and a developer in Japan. I am a member of TecoStudio."
         />
       </Head>
       <Container className="mt-9">
@@ -211,7 +239,7 @@ export default function Home({ articles }) {
             Server Developer, Translator and Student
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I am JianyueHugo, a.k.a. JianyueLab, Valor, 15 y.o., a student and a developer in Tokyo.
+            I am JianyueHugo, a.k.a. JianyueLab, Valor, 15 y.o., a student and a developer in Japan.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
@@ -256,7 +284,8 @@ export default function Home({ articles }) {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Resume />
+            <Study />
+            <Works />
           </div>
         </div>
       </Container>
