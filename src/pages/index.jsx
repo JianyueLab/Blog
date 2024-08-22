e80f3ea1da100e271ddc0ce520dcf57c7e8eca18
JianyueLab <valor@jianyuelab.cc>
2024-08-22 19:47:09 +0900

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -175,13 +175,13 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I am JianyueHugo, aka Valor, a student and a developer in Japan. I am a member of TecoStudio."
+          content="I am JianyueHugo, aka Valor, a student and a developer in Japan."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Server Developer, Translator and Student
+            Developer, Translator and Student
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I am JianyueHugo, a.k.a. JianyueLab, Valor, 16 y.o., a student and a developer in Japan.
